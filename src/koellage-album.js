153f832f5650f7afe40c70526ba1fb50ea9272dc
Thomas Kölling <spamdoggi@gmail.com>
2015-02-18 00:15:42 +0100

    added gulp with es6 transpiler

---
+++ src/koellage-album.js
@@ -0,0 +1,18 @@
+let {ownerDocument} = document.currentScript;
+
+class Koellage extends HTMLElement {
+
+    constructor() {
+        //currently not called from registerElement -> wait
+        // -> https://w3c.github.io/webcomponents/spec/custom/#dfn-custom-element-constructor-generation
+    }
+
+    createdCallback() {
+        let template = ownerDocument.querySelector('#koellage-album');
+        let clone = document.importNode(template.content, true);
+        this.createShadowRoot().appendChild(clone);
+    };
+}
+
+document.registerElement('koellage-album', Koellage);
+
